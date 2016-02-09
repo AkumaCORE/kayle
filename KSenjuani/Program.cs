f130d6f8b91eca675473cd19c21e533bed72ece5
bruno105 <bruno1057_@hotmail.com>
2016-02-09 18:20:24 -0300

    Updated

--- KSenjuani/Program.cs
+++ KSenjuani/Program.cs
@@ -70,7 +70,7 @@ namespace KSejuani
                 }
 
                 Bootstrap.Init(null);
-                Chat.Print("KSejuani Addon Loading Successs", Color.Green);
+                Chat.Print("KSejuani Addon Loading Success", Color.Green);
 
                 Q = new Spell.Skillshot(SpellSlot.Q, 650, SkillShotType.Linear, 0, 1600, 70);
                 W = new Spell.Active(SpellSlot.W, 350);
@@ -109,7 +109,7 @@ namespace KSejuani
                 ModesMenu2.Add("LastQ", new CheckBox("Use Q on LastHit", true));
                 ModesMenu2.Add("LastW", new CheckBox("Use W on LastHit", true));
                 ModesMenu2.Add("LastE", new CheckBox("Use E on LastHit", true));
-                ModesMenu2.AddLabel("Lane Cler Config");
+                ModesMenu2.AddLabel("Lane/Jungle Clear Config");
                 ModesMenu2.Add("ManaF", new Slider("Dont use Skills if Mana <=", 40));
                 ModesMenu2.Add("FarmQ", new CheckBox("Use Q on LaneClear", true));
                 ModesMenu2.Add("FarmW", new CheckBox("Use W on LaneClear", true));
@@ -184,113 +184,33 @@ namespace KSejuani
         static void Game_OnUpdate(EventArgs args)
         {
 
-
+            
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
             {
-                var alvo = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
-                var rmax = EntityManager.Heroes.Enemies.Where(t => t.IsInRange(Player.Instance.Position, R.Range) && !t.IsDead && t.IsValid && !t.IsInvulnerable).Count();
-                if (!alvo.IsValid()) return;
-                var predPos = Prediction.Position.PredictLinearMissile(alvo, R.Range, R.Width, R.CastDelay, R.Speed,int.MaxValue, null, false);
-                
-                
-                if (Q.IsReady() && Q.IsInRange(alvo) && ModesMenu1["ComboQ"].Cast<CheckBox>().CurrentValue)
-                {
-                    Q.Cast(alvo);
-                }
-                if (W.IsReady() && W.IsInRange(alvo) && ModesMenu1["ComboW"].Cast<CheckBox>().CurrentValue)
-                {
-                    W.Cast();
-
-                }
-                if (E.IsReady() && E.IsInRange(alvo) && ModesMenu1["ComboE"].Cast<CheckBox>().CurrentValue)
-                {
-                    E.Cast();
-
-                }
-                if (R.IsReady() && R.IsInRange(alvo) && ModesMenu1["ComboR"].Cast<CheckBox>().CurrentValue && (rmax >= ModesMenu1["MinR"].Cast<Slider>().CurrentValue) && predPos.HitChance > HitChance.High    )
-                {
-                     R.Cast(predPos.CastPosition);
-
-                }
-
-
+                ModesManager.Combo();
             }
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
             {
-                var alvo = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
-                if (!alvo.IsValid()) return;
-
-
-
-                if ((_Player.ManaPercent <= ModesMenu1["ManaH"].Cast<Slider>().CurrentValue))
-                {
-                    return;
-                }
-                if (Q.IsReady() && Q.IsInRange(alvo) && ModesMenu1["HarassQ"].Cast<CheckBox>().CurrentValue)
-                {
-                    Q.Cast(alvo);
-                }
-                if (W.IsReady() && W.IsInRange(alvo) && ModesMenu1["HarassW"].Cast<CheckBox>().CurrentValue)
-                {
-                    W.Cast();
-
-                }
-                if (E.IsReady() && E.IsInRange(alvo) && ModesMenu1["HarassE"].Cast<CheckBox>().CurrentValue)
-                {
-                    E.Cast();
-
-                }
-
-
+                ModesManager.Harass();
             }
 
-            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear) || Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear) )
             {
 
-
-                var minions = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(Q.Range) && (DamageLib.QCalc(m) > m.Health));
-                var minion = EntityManager.MinionsAndMonsters.EnemyMinions.Where(t => t.IsInRange(Player.Instance.Position, E.Range) && !t.IsDead && t.IsValid && !t.IsInvulnerable).Count();
-                if (minions == null) return;
-                if ((_Player.ManaPercent <= ModesMenu2["ManaF"].Cast<Slider>().CurrentValue))
-                {
-                    return;
-                }
-                if (Q.IsReady() && Q.IsInRange(minions) && ModesMenu2["FarmQ"].Cast<CheckBox>().CurrentValue && minions.Health < DamageLib.QCalc(minions))
-                {
-                    Q.Cast(minions);
-                }
-
-                if (W.IsReady() && W.IsInRange(minions) && ModesMenu2["FarmW"].Cast<CheckBox>().CurrentValue )
-                {
-                    W.Cast();
-
-                }
-                if (E.IsReady() && E.IsInRange(minions) && ModesMenu1["FarmE"].Cast<CheckBox>().CurrentValue && (minion >= ModesMenu2["MinionE"].Cast<Slider>().CurrentValue))
-                {
-                    E.Cast();
-
-                }
+                ModesManager.LaneClear();
+           
             }
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            {
+
+                ModesManager.JungleClear();
+            }
+
+
+
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit))
             {
-                var qminions = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(Q.Range) && (DamageLib.QCalc(m) > m.Health));
-                var wminions = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(W.Range) && (DamageLib.WCalc(m) > m.Health));
-                var eminions = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(E.Range) && (DamageLib.ECalc(m) > m.Health));
-                var minion = EntityManager.MinionsAndMonsters.EnemyMinions.Where(t => t.IsInRange(Player.Instance.Position, E.Range) && !t.IsDead && t.IsValid && !t.IsInvulnerable).Count();
-                if (qminions == null) return;
-                var prediction = Q.GetPrediction(qminions);
-                if (Q.IsReady() && Q.IsInRange(qminions) && ModesMenu2["LastQ"].Cast<CheckBox>().CurrentValue && qminions.Health < DamageLib.QCalc(qminions))
-                {
-                    Q.Cast(qminions);
-                }
-                if (W.IsReady() && W.IsInRange(wminions) && ModesMenu2["LastW"].Cast<CheckBox>().CurrentValue && wminions.Health < DamageLib.WCalc(wminions) && (minion >= ModesMenu2["MinionW"].Cast<Slider>().CurrentValue))
-                {
-                    W.Cast(wminions);
-                }
-                if (W.IsReady() && E.IsInRange(wminions) && ModesMenu2["LastE"].Cast<CheckBox>().CurrentValue && eminions.Health < DamageLib.ECalc(wminions) && (minion >= ModesMenu2["MinionW"].Cast<Slider>().CurrentValue))
-                {
-                    W.Cast(wminions);
-                }
+                ModesManager.LastHit();
 
             }
 
